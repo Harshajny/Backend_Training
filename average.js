41e7cae63810f5b9f681a898837e06e888c56240
Harsha <harshajny@gmail.com>
2024-07-03 09:01:31 +0530

    js files

---
+++ average.js
@@ -0,0 +1,22 @@
+function average(a,b)
+{
+    a=parseFloat(a);
+    b=parseFloat(b);
+    return("abc");
+
+}
+
+console.log(average(10.3,34));
+
+
+
+//package version : major.minor.patch
+//patch : bug fixes
+//minor:doesnt break the previous code
+//major: breaks the aldready exisiting code
+
+
+// express is a package that has its own dependencies along with the dependencies that our package has
+
+//though we were expecting a number we recieved a string. here the importance of typescript comes in 
+//we define the type of everything including the function excpecting return type so that there wont be any compilation error or run time eror
